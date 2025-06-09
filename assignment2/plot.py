7a66818909a5d6e38fcdc4524750a2a6156582a9
Saenai <3345930559@qq.com>
2025-06-09 16:56:10 +0100

    work2

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -3,18 +3,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # parameters to modify
-filename="test.data"
-label='label'
-xlabel = 'xlabel'
-ylabel = 'ylabel'
-title='Simple plot'
-fig_name='test.png'
+filename="ping00001p.txt"
+label='RRT'
+xlabel = 'Time(s)'
+ylabel = 'CDF'
+title='CDF of RRT (0.0001s)'
+fig_name='00001RRT.png'
 bins=100 #adjust the number of bins to your plot
 
 
 t = np.loadtxt(filename, delimiter=" ", dtype="float")
-
-plt.plot(t[:,0], t[:,1], label=label)  # Plot some data on the (implicit) axes.
+data = t[:, 1]
+data = np.sort(data)
+cdf = np.arange(len(data)) / len(data)
+plt.plot(data, cdf, label=label)  # Plot some data on the (implicit) axes.
 #Comment the line above and uncomment the line below to plot a CDF
 #plt.hist(t[:,1], bins, density=True, histtype='step', cumulative=True, label=label)
 plt.xlabel(xlabel)
